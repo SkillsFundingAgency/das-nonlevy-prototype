cb86c824c57eca3bce18c4fc10a0edb3bffaf5fa
rob <rob@Unknown.local>
2018-08-24 16:30:27 +0100

    updates

--- app/routes.js
+++ app/routes.js
@@ -10,57 +10,6 @@ router.use(function(req, res, next) {
 });
 
 
-// Start Dummy Data
-const dummy_employer_1 = {
-  id: "XJBMNV",
-  name: "Nice Employer"
-}
-
-const dummy_employer_2 = {
-  id: "PPJTRA",
-  name: "Good Employer"
-}
-
-router.use(function (req, res, next) {
-//  if (!req.session.cohorts) {
-  //  console.log("Adding cohorts to session");
-    //req.session.cohorts = [dummy_cohort_1, dummy_cohort_2, dummy_cohort_3, dummy_cohort_4, dummy_cohort_5];
- // }
-  if (!req.session.employers) {
-    console.log("Adding employers to session");
-    req.session.employers = [dummy_employer_1, dummy_employer_2];
-  }
-//  if (!req.session.apprentices) {
-//    console.log("Adding apprentices to session");
-//    req.session.apprentices = [dummy_apprentice_1, dummy_apprentice_2, dummy_apprentice_3, dummy_apprentice_4, dummy_apprentice_5];
-//  }
-  // if (!req.session.transactions) {
-  //   console.log("Adding transactions to session");
-  //   req.session.transactions = [
-  //     dummy_transaction_a1, dummy_transaction_a2, dummy_transaction_a3, dummy_transaction_a4,
-  //     dummy_transaction_b1, dummy_transaction_b2, dummy_transaction_b3, dummy_transaction_b4,
-  //     dummy_transaction_c1, dummy_transaction_c2, dummy_transaction_c3, dummy_transaction_c4,
-  //     dummy_transaction_d1, dummy_transaction_d2, dummy_transaction_d3, dummy_transaction_d4,
-  //     dummy_transaction_e1, dummy_transaction_e2, dummy_transaction_e3, dummy_transaction_e4,
-  //     dummy_transaction_f1, dummy_transaction_f2, dummy_transaction_f3, dummy_transaction_f4,
-  //     dummy_transaction_g1, dummy_transaction_g2, dummy_transaction_g3, dummy_transaction_g4,
-  //     dummy_transaction_h1, dummy_transaction_h2, dummy_transaction_h3, dummy_transaction_h4,
-  //     dummy_transaction_i1, dummy_transaction_i2, dummy_transaction_i3, dummy_transaction_i4,
-  //     dummy_transaction_j1, dummy_transaction_j2, dummy_transaction_j3, dummy_transaction_j4,
-  //     dummy_transaction_k1, dummy_transaction_k2, dummy_transaction_k3, dummy_transaction_k4,
-  //     dummy_transaction_l1, dummy_transaction_l2, dummy_transaction_l3, dummy_transaction_l4,
-  //     dummy_transaction_m1, dummy_transaction_m2, dummy_transaction_m3, dummy_transaction_m4,
-  //   ];
-  // }
- 
- // if (!req.session.connectionRequests) {
- //   console.log("Adding connectionRequests to session");
- //   req.session.connectionRequests = [];
- // }
-  next();
-})
-
-
 // Get Sprint and Feature for URL links
 router.use('/', (req, res, next) => {
   req.version = req.originalUrl.split('/')[1] // this is added by DC project
@@ -73,6 +22,7 @@ router.use('/', (req, res, next) => {
   next()
 })
 
+
 // Versions routing stuff - so indivdual routes are in the sub version
 router.use(/\/version-([0-9]+)/, (req, res, next) => {
   require(`./views/version-${req.params[0]}/routes`)(req, res, next);
@@ -82,6 +32,46 @@ console.log('main routes loaded');
 router.get('/', (req, res) => {
   res.render('index')
 })
+
+/// Employers ID setup
+
+// Start Dummy Data
+const dummy_employer_1 = {
+  id: "XJBMNV",
+  name: "Assurance Aerospace Engineering"
+}
+
+const dummy_employer_2 = {
+  id: "PPJTRA",
+  name: "; DROP TABLE \"COMPANIES\";-- LTD"
+}
+
+
+router.use(function (req, res, next) {
+  if (!req.session.employers) {
+    console.log("Adding employers to session");
+    req.session.employers = [dummy_employer_1, dummy_employer_2];
+  }
+  next();
+})
+
+
+router.param('employer', function (req, res, next, employer) {
+  var employers = req.session.employers.filter(e => e.id == employer);
+  if (employers.length == 1) {
+    res.locals.employer = employers[0];
+  }
+  next();
+});
+
+// Employer
+// router.get('/version-1/:employer', function (req, res) {
+ // res.render('version-1/home/index');
+// })
+
+
+
+///// End of employers ID stuff
 
 
 // routing the app 
@@ -230,7 +220,7 @@ router.get('/version-1/find/chooseFATType' , function (req, res) {
            break;
 
           case  (fatType == 'provider'):
-              res.redirect(`/${req.version}/find/providersearch`)
+              res.redirect(`/${req.version}/find/findaprovider`)
           break;
 
         default:
@@ -240,11 +230,71 @@ router.get('/version-1/find/chooseFATType' , function (req, res) {
 })
 
 
+/// Add apprentices > confirm the training provider
+router.get('/version-1/apprentices/add/confirming-training-provider' , function (req, res) {
+  var confirmPro = req.query.confirmation
+       switch (true) {
+          case  (confirmPro == 'true'):
+            res.redirect(`/${req.version}/apprentices/add/start-adding-apprentices`)
+           break;
+
+          case  (confirmPro == 'false'):
+              res.redirect(`/${req.version}/apprentices/add/training-provider`)
+          break;
+
+        default:
+             console.log('gyahhhhhhhh, bork bork borka')
+            break;
+        }
+})
+
+
+
+/// Add apprentices > add apprentices yourself or send to provider
+router.get('/version-1/apprentices/add/addingApprentices' , function (req, res) {
+  var confirmAddApps = req.query.addApprentices
+       switch (true) {
+          case  (confirmAddApps == 'true'):
+            res.redirect(`/${req.version}/apprentices/add/review-cohort`)
+           break;
+
+          case  (confirmAddApps == 'false'):
+              res.redirect(`/${req.version}/apprentices/add/message-for-training-provider`)
+          break;
+
+        default:
+             console.log('gyahhhhhhhh, bork bork borka')
+            break;
+        }
+})
+
+/// Add apprentices > add apprentices yourself or send to provider
+// router.get('/version-1/apprentices/add/finishAppEarly' , function (req, res) {
+ // var optionFinishApps = req.query.optionFinishApps
+   //    switch (true) {
+     //     case  (optionFinishApps == 'send'):
+       //     res.redirect(`/${req.version}/apprentices/cohorts/index`)
+         //  break;
+
+      //    case  (optionFinishApps == 'save'):
+        //      res.redirect(`/${req.version}/apprentices/cohorts/index`)
+          // break;
+
+      //  default:
+      //       console.log('gyahhhhhhhh, bork bork borka')
+      //      break;
+      //  }
+// })
+
+
+
 /// registration > what you'll need
 //// Not using routes for this as has a weird behaviour on the no option, so dealt with in page.
 /// manage-apprenticeships/whatyoullneed
 
 /// registration > 
+
+
 
 
 module.exports = router
