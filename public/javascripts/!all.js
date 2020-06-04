e0eb991058a7658e29d38ec09a1a108cddf39758
rob <rob@Unknown.lan>
2020-06-04 13:40:58 +0100

    update

--- public/javascripts/!all.js
+++ public/javascripts/!all.js
@@ -2088,11 +2088,16 @@ Tabs.prototype.init = function () {
   }
 };
 
+//
 Tabs.prototype.setupResponsiveChecks = function () {
-  this.mql = window.matchMedia('(min-width: 40.0625em)');
+  //Change by RJ on 11 Feb to make tabs work as tabs on mobile
+  
+//  this.mql = window.matchMedia('(min-width: 40.0625em)');
+    this.mql = window.matchMedia('(min-width: 1.0625em)');
   this.mql.addListener(this.checkMode.bind(this));
   this.checkMode();
 };
+//
 
 Tabs.prototype.checkMode = function () {
   if (this.mql.matches) {
